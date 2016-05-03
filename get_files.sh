c2d4d9560350d8091deea696577905cf9e0892a7
Olav <olav@comoyo.com>
2016-05-03 12:37:46 +0200

    Added download script for dependency files

---
+++ get_files.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ ! -f "imagenet-vgg-verydeep-19.mat" ]; then
+    echo "Downloading VGG imagenet weights"
+    curl http://www.vlfeat.org/matconvnet/models/imagenet-vgg-verydeep-19.mat > imagenet-vgg-verydeep-19.mat
+else
+    echo "VGG imagenet weights already found"
+fi
+if [ ! -d "train2014" ]; then
+    echo "Downloading COCO image dataset"
+    curl http://msvocds.blob.core.windows.net/coco2014/train2014.zip > train2014.zip
+    unzip train2014.zip
+    rm train2014.zip
+else
+    echo "Already found COCO image dataset"
+fi
